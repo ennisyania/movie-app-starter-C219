fb0bf328f4e34ff37f766ab68be28dbb573074a9
Enni Sya'nia <ennisyania@gmail.com>
2025-01-08 08:08:19 +0000

    Completed

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import { useEffect, useState } from "react";
-const KEY = "";
+const KEY = "ea64bc1c";
 
 function App() {
   const [movies, setMovies] = useState([]);
+  const [query, setQuery] = useState("batman");
 
-  fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=batman`)
-    .then((res) => res.json())
-    .then((data) => setMovies(data.Search));
+  useEffect(() => {
+    const controller = new AbortController();
+    fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {
+      signal: controller.signal,
+    })
+      .then((res) => res.json())
+      .then((data) => data.Response === "True" && setMovies(data.Search))
+      .catch((err) => console.log(err));
+    return () => controller.abort();
+  }, [query]);
 
   return (
     <div>
       <h1>Movies</h1>
-      <input type="text" placeholder="Search movies..." />
+      <input
+        type="text"
+        value={query}
+        onChange={({ target }) => {
+          setQuery(target.value);
+        }}
+        placeholder="Search movies..."
+      />
       <table>
         <thead>
           <tr>
